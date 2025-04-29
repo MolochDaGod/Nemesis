f71ce0bbfb59dbe0c778a87305560fcacfaf38a7
MolochDaDev <90799999+MolochDaGod@users.noreply.github.com>
2025-04-29 08:44:40 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+{
+  "name": "grudgeplatform-frontend",
+  "version": "1.0.0",
+  "dependencies": {
+    "@crossmint/client-sdk-react-ui": "^1.0.0",
+    "firebase": "^10.7.1"
+  },
+  "devDependencies": {
+    "vite": "^4.4.9"
+  }
+}
