e3b648cabeb5c730171ea21fbec46f61a9d0073a
MolochDaDev <90799999+MolochDaGod@users.noreply.github.com>
2025-04-29 08:39:12 -0500

    Create node.js

---
+++ node.js
@@ -0,0 +1,20 @@
+{
+  "name": "grudgeplatform-functions",
+  "version": "1.0.0",
+  "engines": {
+    "node": "18"
+  },
+  "dependencies": {
+    "@google-cloud/storage": "^7.7.0",
+    "axios": "^1.7.2",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "firebase-admin": "^12.1.0",
+    "firebase-functions": "^4.4.1",
+    "@supabase/supabase-js": "^2.39.3"
+  },
+  "devDependencies": {
+    "eslint": "^8.57.0",
+    "eslint-config-google": "^0.14.0"
+  }
+}
