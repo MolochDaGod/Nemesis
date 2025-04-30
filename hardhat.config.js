bfe371c2e4d699c47178c5c192e0ce7e8046f509
MolochDaDev <90799999+MolochDaGod@users.noreply.github.com>
2025-04-30 10:11:32 +0000

    Pending changes exported from your codespace

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     polygon: {
-      url: "https://polygon-rpc.com", // Replace with your Polygon RPC endpoint
-      accounts: ["YOUR_PRIVATE_KEY"] // Replace with your wallet private key
+      url: "https://polygon-rpc.com/zkevm", // Replace with your Polygon RPC endpoint
+      accounts: ["44e0c590ea4f65e09c50eb4db894ea2e780d7948d6be2cb973a7d639a080afe1"  ] // Replace with your wallet private key
     }
   }
 };require("@nomicfoundation/hardhat-toolbox");
@@ -14,8 +14,8 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     polygon: {
-      url: "https://polygon-rpc.com", // Replace with your Polygon RPC endpoint
-      accounts: ["YOUR_PRIVATE_KEY"] // Replace with your wallet private key
+      url: "https://polygon-rpc.com/zkevm", // Replace with your Polygon RPC endpoint
+      accounts: ["44e0c590ea4f65e09c50eb4db894ea2e780d7948d6be2cb973a7d639a080afe1"] // Replace with your wallet private key
     }
   }
 };
